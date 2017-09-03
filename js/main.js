f1f9b77ea011fb16903883f4a1536381b8fcdf37
Marko <markthomasnoonan@gmail.com>
2017-09-03 07:51:36 -0400

    Show detail view based on hovering on a card

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
-var context = {targets: targets};
+var context = {
+  targets: targets
+};
 
-function render(templateSelector, destinationSelector) {
+function render(templateSelector, destinationSelector, targetIndex) {
   var chosenTemplate = document.querySelector(templateSelector).innerHTML;
   var destination = document.querySelector(destinationSelector);
   var template = Handlebars.compile(chosenTemplate);
-  var html = template(context);
+  if (targetIndex >= 0) {
+    var html = template(context.targets[targetIndex]);
+  } else {
+    var html = template(context);
+  }
   destination.innerHTML = html;
+  if (templateSelector === "#list-template"){
+    addListListeners();
+  }
 }
 
-render('#list-template', '#list-container');
+render('#list-template', '#list');
+
+function showDetail(index) {
+  console.log("detail");
+  render("#detail-template", "#detail-container", index);
+}
+
+function addListListeners(){
+  var listItems = [].slice.call(document.querySelectorAll(".company-list__item"));
+  console.log(listItems);
+
+  listItems.forEach(function(listItem, i){
+    listItem.addEventListener("mouseover",
+      function () {
+        showDetail(i);
+      });
+    });
+  }
