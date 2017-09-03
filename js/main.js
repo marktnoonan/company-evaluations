6efd7a24e4e830f3c567bd03593ab0ef8664d359
Marko <markthomasnoonan@gmail.com>
2017-09-02 22:24:38 -0400

    basic list view template test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-targets.forEach(function (target) {
-console.log(target.companyName);
-});
+var context = {targets: targets};
+
+function render(templateSelector, destinationSelector) {
+  var chosenTemplate = document.querySelector(templateSelector).innerHTML;
+  var destination = document.querySelector(destinationSelector);
+  var template = Handlebars.compile(chosenTemplate);
+  var html = template(context);
+  destination.innerHTML = html;
+}
+
+render('#list-template', '#list-container');
